a748131963a4e8ecee53246f27b1bca0472c3cc6
Vinicius Nunes <vinicnm@gmail.com>
2023-04-07 14:23:25 -0300

    first commit

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "db_name" {
+  description = "DB name"
+  type = string
+}
+
+variable "password" {
+  description = "DB password"
+  type = string
+}
+
+variable "engine_version" {
+  description = "Version"
+  type = string
+}
